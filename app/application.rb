5708c9cdc4fcfd7d40549e8ba415db6619cf01c5
Derek Kaknes <derek.kaknes@gmail.com>
2016-12-29 18:21:53 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,18 +1,44 @@
+class NoItemError < StandardError
+end
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
+  def display_cart
+    if @@cart.empty?
+      "Your cart is empty"
+    else
+      @@cart.join("\n")
+    end
+  end
+
+  def add_item(item)
+    if @@items.include?(item)
+      @@cart << item
+      "added #{item}"
+    else
+      "We don't have that item"
+    end
+  end
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
-    if req.path.match(/items/)
+    resp.write("Path = #{req.path}\n\n\n")
+    if req.path.eql?('/')
+      resp.write("Welcome to the store!")
+    elsif req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
       end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      resp.write(display_cart)
+    elsif req.path.match(/add/)
+      item = req.params["item"]
+      resp.write(add_item(item))
     else
       resp.write "Path Not Found"
     end
