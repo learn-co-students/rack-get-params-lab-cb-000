12bc3538cd26e5dc41a1747ec75d24c1c6825c5c
Jen Poole <contact.vandepool@gmail.com>
2018-02-25 03:07:26 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,9 @@ class Application
 
   @@items = ["Apples","Carrots","Pears"]
 
+  # Create a new class array called @@cart to hold any items in your cart
+  @@cart = []
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -13,6 +16,18 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
+    # Create a new route called /cart to show the items in your cart
+    elsif req.path.match(/cart/)
+      # responds with empty cart message if the cart is empty
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      # responds with a cart list if there is something in there  
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
     else
       resp.write "Path Not Found"
     end
