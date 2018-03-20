e7cef6b3d4148d4fcc5ed33dc1cb48f9761d5d0e
adam n <adam.nawroc@gmail.com>
2018-03-20 17:45:55 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -13,6 +14,28 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
+    elsif req.path.match(/add/)
+      toAdd = req.params["item"]
+
+
+      if @@items.include? toAdd
+        @@cart << toAdd
+        # dont see whats wrong here lul
+        resp.write "added #{toAdd}"
+      else
+        resp.write "We don't have that item"
+
+      end
+
+    elsif req.path.match(/cart/)
+
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        # @@cart.each {|item| puts item}
+        resp.write @@cart.join("\n")
+      end
     else
       resp.write "Path Not Found"
     end
